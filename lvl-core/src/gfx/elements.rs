e0b42fb56f288285de52592dadbcadd0c2766305
AcrylicShrimp <led789zxpp@naver.com>
2024-05-09 19:39:41 +0900

    refactor: drop unused

--- lvl-core/src/gfx/elements.rs
+++ lvl-core/src/gfx/elements.rs
@@ -2,12 +2,10 @@ mod material;
 mod pmx_model;
 mod shader;
 mod shader_reflection;
-mod static_mesh;
 mod texture;
 
 pub use material::*;
 pub use pmx_model::*;
 pub use shader::*;
 pub use shader_reflection::*;
-pub use static_mesh::*;
 pub use texture::*;
